46f2e5f245783746efb3daf62dcbd5f81f7bd263
avishay <avishayhajbi@gmail.com>
2015-05-21 06:15:52 +0300

    done functionality

--- index.js
+++ index.js
@@ -25,26 +25,90 @@ app.get('/', function(req, res){
   res.render('index', { 
   	title: "Shoes store",
   	structure: 'id, size, company, useFor',
-  	info: ['GET getShoeById',
+  	info: ['GET getAllShoes',
+    'GET getShoeById',
     'GET insertNewShoe',
     'GET deleteShoeById',
-    'GET findShow'],
-  	desc: ['example: /getShoeById/id = 123 ',
-    'example: /insertNewShoe/id = 123 & size = 42 & company = nike & useFor = running',
-  	'example: /deleteShoeById/id = 123 ',
-    'example: findShow/id = 123 & size = 42 & useFor = walking & company = nike']
+    'GET findShow',
+    'GET updateShoe'],
+  	desc: ['example: /getAllShoes',
+    'example: /getShoeById/?id=123',
+    'example: /insertNewShoe/?id=123&size=42&company=nike&useFor=running',
+  	'example: /deleteShoeById/?id=123',
+    'example: /findShoe/?id=123&size=42&company=nike&useFor=running',
+    'example: /updateShoe/?id=123&size=42&company=nike&useFor=running']
   });
 });
 
-app.get('/getShoeById/:id?', function(req, res){
-  var temp = shoes.getShoeById("123");
-  res.josn(temp);
+app.get('/getAllShoes', function(req, res){
+  res.json(shoes.getAllShoes());
 });
 
+app.get('/getShoeById/:id?', function(req, res){
+  var id= req.query.id || 0;
+  console.log("search shoe id",id)
+  var temp = shoes.getShoeById(id);
+  res.json(temp);
+});
+
+app.get('/insertNewShoe/:id?:size?:company?:useFor?', function(req, res){
+  var data = {
+    id: req.query.id || new Date().getTime(),
+    size: req.query.size || 1,
+    company: req.query.company || '',
+    useFor: req.query.useFor || ''
+  }
+  console.log("insert new shoe",data)
+  var temp = shoes.insertShoe(data);
+  res.json(temp);
+});
+
+
+app.get('/deleteShoeById/:id?', function(req, res){
+  var id= req.query.id || 0;
+  console.log("delete shoe id",id)
+  var temp = shoes.deleteShoe(id);
+  res.json(temp);
+});
+
+app.get('/updateShoe/:id?:size?:company?:useFor?', function(req, res){
+  var data = {
+    id: req.query.id,
+    size: req.query.size,
+    company: req.query.company,
+    useFor: req.query.useFor
+  };
+  if (!data.id) {
+    console.log("update shoe - id is missing")
+    res.json({status:0,desc:"parameter id is missing"});
+    return;
+  }
+  console.log("update shoe",data);
+  var temp = shoes.updateShoe(data);
+  res.json(temp);
+});
+
+app.get('/findShoe/:id?:size?:company?:useFor?', function(req, res){
+  var data = {
+    id: req.query.id,
+    size: req.query.size,
+    company: req.query.company,
+    useFor: req.query.useFor
+  };
+  if (!data.id && !data.size && !data.company && !data.useFor ) {
+    console.log("findShoe shoe - id is missing")
+    res.json({status:0,desc:"no parameters found"});
+    return;
+  }
+  console.log("findShoe shoe",data);
+  var temp = shoes.findShoe(data);
+  res.json(temp);
+});
 
 app.listen(app.get('port'), function() {
   console.log('Server running...' + app.get('port'));
 });
+
 
 app.get('/*', function(req, res) {
   res.send(405, 'WS can not find page');
--- shoes/index.js
+++ shoes/index.js
@@ -15,6 +15,10 @@ function Shoes (){
   this.index = [];
 }
 
+Shoes.prototype.getAllShoes = function (){
+  return this.shoes;
+}
+
 Shoes.prototype.init = function (){
   var temp = { id: "123" , size: "42"};
   this.shoes.push(new Shoe(temp));
@@ -29,7 +33,6 @@ Shoes.prototype.init = function (){
   this.shoes.push(new Shoe(temp));
   this.index.push(temp.id);
   console.log(this.shoes);
-
 }
 
 Shoes.prototype.insertShoe = function (data){
@@ -38,34 +41,57 @@ Shoes.prototype.insertShoe = function (data){
   var shoe = new Shoe(data);
   this.shoes.push(shoe);
   this.index.push(shoe.id);
-  console.log("new shoe created", shoe)
+  console.log("new shoe created", shoe);
+  return ({status:1,desc:"success shoe "+shoe.id+" created"});
 }
 
 Shoes.prototype.updateShoe = function (data){
   var index = this.index.indexOf(data.id);
-  if (index == -1) return;
-  var shoe = new Shoe(data)
-  this.shoes[index] = shoe;
+  if (index == -1) return ({status:0,desc:"shoe "+data.id+" not found"});
+  this.shoes[index].size=(data.size) ? data.size : this.shoes[index].size;
+  this.shoes[index].company=(data.company) ? data.company : this.shoes[index].company;
+  this.shoes[index].useFor=(data.useFor) ? data.useFor : this.shoes[index].useFor;
   console.log("shoe "+data.id+" updated")
+  return ({status:1,desc:"shoe "+data.id+" was updated",res:this.shoes[index]});
 }
 
-Shoes.prototype.deleteShoe = function (data){
-  var index = this.index.indexOf(data.id);
-  if (index == -1) return;
-  this.shoes.slice(index,1);
-  this.index.slice(index,1);
-  console.log("shoe "+data.id+" deleted")
+Shoes.prototype.deleteShoe = function (id){
+  var index = this.index.indexOf(id);
+  if (index == -1) return ({status:0,desc:"shoe "+id+" not found"});
+  this.shoes.splice(index,1);
+  this.index.splice(index,1);
+  console.log("shoe "+id+" deleted");
+  return ({status:1,desc:"success shoe "+id+" deleted"});
 }
 
 Shoes.prototype.getShoeById = function (id){
   var index = this.index.indexOf(id);
-  if (index == -1) return;
+  if (index == -1) return ({status:0,desc:"shoe "+id+" not found"});
   console.log("shoe "+id+" found",this.shoes[index] );
-  return this.shoes[index];
+  return ({status:1,desc:"shoe "+id+" found",res:this.shoes[index]});
 }
 
-Shoes.prototype.findShoe = function (id){
-  
+Shoes.prototype.findShoe = function (data){
+  var temp = [];
+  temp=this.shoes.filter(function(v){
+    return data.id ||v.id == v.id && 
+    data.size ||v.size== v.size && 
+    data.company ||v.company== v.company && 
+    data.useFor ||v.useFor== v.useFor    
+  });
+  /*
+  var temp=this.shoes.filter(function(v){
+    return data.id == v.id && 
+    data.size == v.size && 
+    data.company == v.company && 
+    data.useFor == v.useFor;   
+  });
+    data.id == v.id && 
+    data.size == v.size && 
+    data.company == v.company && 
+    data.useFor == v.useFor;
+  */
+  return ({status:1,desc:temp.length+" shoes found",res:temp});
 }
 
 module.exports = Shoes;
